53b21119ea3a1520cb034ac90c1a571cb077368e
vasudhashah <vasudhashahbpr@gmail.com>
2023-07-25 23:21:36 +0530

    Stack using LinkedList

---
+++ Stack/StackUsingLinkedList/StackUsingLinkedList/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LearnDSAlgorithms
+{
+    public class Node
+    {
+        public int element;
+        public Node next;
+
+        public Node(int e, Node n)
+        {
+            element = e;
+            next = n;
+        }
+    }
+
+    class StacksLinked
+    {
+        Node top;
+        int size;
+
+        public StacksLinked()
+        {
+            top = null;
+            size = 0;
+        }
+
+        public int length()
+        {
+            return size;
+        }
+
+        public bool isEmpty()
+        {
+            return size == 0;
+        }
+
+        public void push(int e)
+        {
+            Node newest = new Node(e, null);
+            if (isEmpty())
+            {
+                top = newest;
+            }
+            else
+            {
+                newest.next = top;
+                top = newest;
+            }
+            size = size + 1;
+        }
+
+        public int pop()
+        {
+            if (isEmpty())
+            {
+                Console.WriteLine("Stack is empty");
+                return -1;
+            }
+            int e = top.element;
+            top = top.next;
+            size = size - 1;
+            return e;
+        }
+
+        public int peek()
+        {
+            if (isEmpty())
+            {
+                Console.WriteLine("Stack is empty");
+                return -1;
+            }
+            return top.element;
+        }
+
+        public void display()
+        {
+            Node p = top;
+            while (p != null)
+            {
+                Console.Write(p.element + "-->");
+                p = p.next;
+            }
+            Console.WriteLine();
+        }
+
+        static void Main(string[] args)
+        {
+            StacksLinked s = new StacksLinked();
+            s.push(5);
+            s.push(3);
+            s.display();
+            Console.WriteLine("Size: " + s.length());
+            Console.WriteLine("Element Removed: " + s.pop());
+            Console.WriteLine("IsEmpty: " + s.isEmpty());
+            Console.WriteLine("Element Removed: " + s.pop());
+            Console.WriteLine("IsEmpty: " + s.isEmpty());
+            s.push(7);
+            s.push(9);
+            s.display();
+            Console.WriteLine("Element Top: " + s.peek());
+            s.display();
+            Console.ReadKey();
+        }
+    }
+}
